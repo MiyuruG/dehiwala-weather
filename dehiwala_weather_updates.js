8dd387e3516fea5a88a723a1a31ddc6645ce5101
MiyuruG <rohangunatilake@gmail.com>
2025-01-14 20:42:20 +0000

    Update weather

--- dehiwala_weather_updates.js
+++ dehiwala_weather_updates.js
@@ -1,1 +1,1 @@
-const dehiwalaWeatherData = {"temp": 23.76, "humidity": 94, "description": "overcast clouds", "updatedAt": 1736886938};
+const dehiwalaWeatherData = {"temp": 23.76, "humidity": 94, "description": "overcast clouds", "updatedAt": 1736887340};
